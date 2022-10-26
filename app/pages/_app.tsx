fa4e4589f5cbbc950839ed2c15b8c4892aee9a3d
James Spencer <me@jspenc.com>
2022-10-26 22:30:51 +0100

    ui template added

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app">
+      <header>
+        <h1>Link shortener</h1>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer className="footer">James Spencer</footer>
+    </div>
+  );
 }
