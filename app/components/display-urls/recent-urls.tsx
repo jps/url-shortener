f285345b2110dc15cebec8ae8e70e7462274f790
James Spencer <me@jspenc.com>
2022-10-31 09:31:40 +0000

    minor ui tweaks

--- app/components/display-urls/recent-urls.tsx
+++ app/components/display-urls/recent-urls.tsx
@@ -3,7 +3,7 @@ export const RecentUrls = (props: { urls: string[] }) => (
     <h2>Recent URLs</h2>
     <ul className="recent-urls__list">
       {props.urls.map((url, i) => (
-        <li key={i}>
+        <li key={i} className="recent-urls__list-item">
           <a href={url}>{url}</a>
         </li>
       ))}
