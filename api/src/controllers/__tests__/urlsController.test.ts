4d8c43804b7261d2afe2bdd54cd853aefe39344b
James Spencer <me@jspenc.com>
2022-11-05 15:58:42 +0000

    update urls controller

--- api/src/controllers/__tests__/urlsController.test.ts
+++ api/src/controllers/__tests__/urlsController.test.ts
@@ -1,91 +1,67 @@
 import { getMockReq, getMockRes } from "@jest-mock/express";
 import { postUrl } from "../urls-controller";
 import { saveAsShortenedUrl } from "../../services";
+import {
+  saveAsShortenedUrlFailureResponse as SaveAsShortenedUrlFailureResponse,
+  SaveAsShortenedUrlSuccessResponse as SaveAsShortenedUrlSuccessResponse,
+} from "../../services/url-shortener-service";
 
 jest.mock("../../services");
 
 describe("urlsController", () => {
   describe("postUrl", () => {
     const { res } = getMockRes();
-    const getPostUrlRequest = (url: string) =>
+    const getPostUrlRequest = () =>
       getMockReq({
         body: {
-          url: url,
+          url: "https://www.google.com",
         },
       });
 
     const mockedSaveAsShortenedUrl = jest.mocked(saveAsShortenedUrl);
 
-    const validUrls = [
-      "https://jspenc.com",
-      "http://jspenc.com",
-      "//jspenc.com",
+    const scenarios = [
+      [
+        { status: "Success", shortenedUrl: "http://jspenc.com/abcdefgh" },
+        200,
+        {
+          shortenedUrl: "http://jspenc.com/abcdefgh",
+        },
+      ],
+      [
+        { status: "ValidationFailed", message: "Invalid url" },
+        400,
+        {
+          message: "Invalid request",
+        },
+      ],
+      [
+        { status: "FailedToSave", message: "Server error" },
+        500,
+        {
+          message: "Failed to save url",
+        },
+      ],
     ];
 
-    const invalidUrls = [
-      "invalidurl",
-      "http://invalidurl",
-      "ftp://invalidurl",
-      "1111",
-      {} as string,
-      undefined as string,
-    ];
-
-    test.each(validUrls)(
-      "should return a 200 if the provided url (%s) is valid and saved",
-      async (input) => {
-        const req = getPostUrlRequest(input);
+    scenarios.forEach((scenario) => {
+      const [mockedServiceResponse, expectedStatusCode, expectedResult] = scenario;
+      test(`should return statusCode:${expectedStatusCode} when service returns status:${mockedServiceResponse}`, async () => {
+        const req = getPostUrlRequest();
 
         mockedSaveAsShortenedUrl.mockResolvedValue(
-          "http://jspenc.com/abcdefgh"
+          mockedServiceResponse as
+            | SaveAsShortenedUrlSuccessResponse
+            | SaveAsShortenedUrlFailureResponse
         );
 
         await postUrl(req, res);
 
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith(
-          expect.objectContaining({
-            url: "http://jspenc.com/abcdefgh",
-          })
-        );
-        expect(mockedSaveAsShortenedUrl).toHaveBeenCalledWith(input);
+        expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+        expect(res.json).toHaveBeenCalledWith(expectedResult);
+        expect(mockedSaveAsShortenedUrl).toHaveBeenCalledWith(req.body);
         expect(mockedSaveAsShortenedUrl).toHaveBeenCalledTimes(1);
-      }
-    );
-
-    test.each(invalidUrls)(
-      "should return a 400 if the provided url (%s) is invalid",
-      async (input) => {
-        const req = getPostUrlRequest(input);
-
-        await postUrl(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.json).toHaveBeenCalledWith(
-          expect.objectContaining({
-            message: "Invalid request",
-          })
-        );
-        expect(mockedSaveAsShortenedUrl).not.toHaveBeenCalled();
-      }
-    );
-
-    test("should return a 500 if saveAsShortenedUrl throws an error", async () => {
-      const req = getPostUrlRequest(validUrls[0]);
-      mockedSaveAsShortenedUrl.mockImplementation(() => {
-        throw new Error("Server Error");
       });
-
-      await postUrl(req, res);
-
-      expect(res.status).toHaveBeenCalledWith(500);
-      expect(res.json).toHaveBeenCalledWith(
-        expect.objectContaining({
-          message: "Something went wrong",
-        })
-      );
-      expect(mockedSaveAsShortenedUrl).toHaveBeenCalledWith(validUrls[0]);
-      expect(mockedSaveAsShortenedUrl).toHaveBeenCalledTimes(1);
     });
   });
 
