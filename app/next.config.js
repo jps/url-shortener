027737392c11504af48e318f2533e039dc719844
James Spencer <me@jspenc.com>
2022-11-14 18:53:18 +0000

    resolve next startup issue

--- app/next.config.js
+++ app/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    //suppress error coming from next https://github.com/vercel/next.js/discussions/30781
+    const nextMiniCssExtractPluginIdx = config.plugins.findIndex(
+      (p) => p?.constructor?.name === "NextMiniCssExtractPlugin"
+    );
+    nextMiniCssExtractPluginIdx > -1 &&
+      config.plugins.splice(nextMiniCssExtractPluginIdx, 1);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
